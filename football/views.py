bbbef19130425b2a286efa23f9ff960e218abcb9
George Peek <gvpeek@yahoo.com>
2014-07-08 19:06:28 -0500

    Converted roster view and template to use new roster fields.

--- football/views.py
+++ football/views.py
@@ -205,14 +205,30 @@ def show_teams(request, universe_id):
         return HttpResponse(template.render(context))
 
 def show_roster(request, team_id, year):
-        t = Team.objects.get(id=team_id)
-        y = Year.objects.get(universe=t.universe, year=year)
-        r = Roster.objects.get(universe=t.universe, team=t, year=y)
-        roster = [r.qb, r.rb, r.wr, r.og, r.c, r.ot, r.de, r.dt, r.lb, r.cb, r.s, r.k, r.p]
+        team = Team.objects.get(id=team_id)
+        year_obj = Year.objects.get(universe=team.universe, year=year)
+        roster = Roster.objects.get(universe=team.universe, team=team, year=year_obj)
+        roster_list = [(roster.qb, roster.qb_age, roster.qb_rating),
+                  (roster.rb, roster.rb_age, roster.rb_rating),
+                  (roster.wr, roster.wr_age, roster.wr_rating),
+                  (roster.og, roster.og_age, roster.og_rating),
+                  (roster.c, roster.c_age, roster.c_rating),
+                  (roster.ot, roster.ot_age, roster.ot_rating),
+                  (roster.de, roster.de_age, roster.de_rating),
+                  (roster.dt, roster.dt_age, roster.dt_rating),
+                  (roster.lb, roster.lb_age, roster.lb_rating),
+                  (roster.cb, roster.cb_age, roster.cb_rating),
+                  (roster.s, roster.s_age, roster.s_rating),
+                  (roster.k, roster.k_age, roster.k_rating),
+                  (roster.p, roster.p_age, roster.k_rating)]
+        for player, age, rating in roster_list:
+            # print line
+            # for player, age, rating in line:
+                print player, age, rating
         template = loader.get_template('football/roster.html')
         context = RequestContext(request, {
-                'team' : t,
-                'roster' : roster,
+                'team' : team,
+                'roster' : roster_list,
         })
         return HttpResponse(template.render(context)) 
 
